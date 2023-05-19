c4334b1bfc4341f29cb449e9f1ae5d2fc9dd76c7
ZianTsabit <13520165@std.stei.itb.ac.id>
2023-05-19 20:31:33 +0700

    feat: CRUD product

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUsers, addSeller } from '../controllers/user';
+import { getUsers, addSeller, getUser } from '../controllers/user';
 import { auth } from "../middlewares/auth";
 
 export const usersRouter = Router();
 
 usersRouter
     .get('/', getUsers)
-    .post('/add-seller',auth(true), addSeller);
+    .get('/:uuid', auth(true), getUser)
+    .post('/register',auth(true), addSeller);
 
 export default usersRouter;
